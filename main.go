0f1fd18e4641d390e2b785909a2ec2852fa03655
Daniel Dreier <ddreier@gmail.com>
2021-10-29 21:58:56 -0500

    Use BoltDB (bbolt) for persistence

--- main.go
+++ main.go
@@ -4,27 +4,71 @@ import (
 	"fmt"
 	"github.com/uptrace/bunrouter"
 	"github.com/uptrace/bunrouter/extra/reqlog"
+	bolt "go.etcd.io/bbolt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	router := bunrouter.New(
+	db, err := bolt.Open("urlshortener.db", 0666, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, _ = tx.CreateBucketIfNotExists([]byte("urls"))
+
+		err := tx.Bucket([]byte("urls")).Put([]byte("hacktoberfest"), []byte("https://hacktoberfest.digitalocean.com/"))
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Listening on :8888")
+	log.Println(http.ListenAndServe(":8888", router(db)))
+}
+
+func router(db *bolt.DB) *bunrouter.Router {
+	r := bunrouter.New(
 		bunrouter.WithMiddleware(reqlog.NewMiddleware(reqlog.WithVerbose(true))))
-	router.GET("/", func(w http.ResponseWriter, r bunrouter.Request) error {
+	r.GET("/", func(w http.ResponseWriter, r bunrouter.Request) error {
 		_, err := fmt.Fprintln(w, r.Method, r.Route(), r.Params().Map())
 		return err
 	})
-	router.GET("/g/:id", func(w http.ResponseWriter, r bunrouter.Request) error {
-		if r.Param("id") == "hacktoberfest" {
-			http.Redirect(w, r.Request, "https://hacktoberfest.digitalocean.com/", http.StatusTemporaryRedirect)
+	r.GET("/g/:id", func(w http.ResponseWriter, r bunrouter.Request) error {
+		id := r.Param("id")
+
+		var found bool
+		var url string
+
+		err := db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("urls"))
+			v := b.Get([]byte(id))
+
+			if v != nil {
+				found = true
+				url = string(v)
+			}
+
+			return nil
+		})
+		if err != nil {
+			return err
+		}
+
+		if found {
+			http.Redirect(w, r.Request, url, http.StatusTemporaryRedirect)
 			return nil
 		}
 
-		http.Error(w, fmt.Sprintf("URL for %q not found", r.Param("id")), http.StatusNotFound)
+		http.Error(w, fmt.Sprintf("URL for %q not found", id), http.StatusNotFound)
 		return nil
 	})
 
-	log.Println("Listening on :8888")
-	log.Println(http.ListenAndServe(":8888", router))
+	return r
 }
